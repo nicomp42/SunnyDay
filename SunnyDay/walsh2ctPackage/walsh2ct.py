b125aafbc4f1bd129fbff260f4ba169ed6b77e30
walsh <walsh@LAPTOP-J239KI4O.wifi.uc.edu>
2024-02-15 13:01:12 -0500

    First Commit

---
+++ SunnyDay/walsh2ctPackage/walsh2ct.py
@@ -0,0 +1,19 @@
+# walsh2ct.py
+# Name: Connor Walsh
+# email: walsh2ct@mail.uc.edu
+# Assignment Number: N/A
+# Due Date: 2/15
+# Course/Section: IS4010-001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: this is an in class assignment utilizing github
+
+# Brief Description of what this module does. Do not copy/paste from a previous assignment. Put some thought into this. this creates a method that prints my name
+# Citations:
+# Anything else that's relevant:
+def walsh2ct():
+    '''
+    prints name
+    :param: none
+    :returns: none
+    '''
+    print("Connor Walsh")
